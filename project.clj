ee15685d0868330e4b98a7202063014386c26689
Jacek Laskowski <jacek@japila.pl>
2012-01-25 14:32:41 +0100

    just before deploying to Heroku for the very first time - http://librarian-clojure.herokuapp.com/

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject librarian-clojure "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+(defproject librarian-clojure "0.0.1"
+  :description "Book manager in Clojure"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [ring/ring-jetty-adapter "1.0.1"]])
--- src/librarian_clojure/core.clj
+++ src/librarian_clojure/core.clj
@@ -1,1 +1,10 @@
-(ns librarian-clojure.core)
+(ns librarian-clojure.core
+  (:use ring.adapter.jetty))
+
+(defn app [req]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello, world"})
+
+(defn -main [port]
+  (run-jetty app {:port (Integer. port)}))
