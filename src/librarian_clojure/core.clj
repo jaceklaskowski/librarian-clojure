5e9cdbfb3f23a438ef968c7bbdeede7da667093c
Jacek Laskowski <jacek@japila.pl>
2012-01-25 13:56:06 +0100

    first commit (after lein new)

---
+++ src/librarian_clojure/core.clj
@@ -0,0 +1,1 @@
+(ns librarian-clojure.core)
---
+++ test/librarian_clojure/test/core.clj
@@ -0,0 +1,6 @@
+(ns librarian-clojure.test.core
+  (:use [librarian-clojure.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
