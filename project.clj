1e12914bcd3e5422e3a7f5661309197a59d608ec
Jacek Laskowski <jacek@japila.pl>
2012-01-29 21:47:58 +0100

    More Ring

--- project.clj
+++ project.clj
@@ -1,4 +1,15 @@
-(defproject librarian-clojure "0.0.1"
+;; http://corfield.org/blog/post.cfm/testing-your-project-against-multiple-versions-of-clojure
+(def common-deps '[[ring/ring-jetty-adapter "1.0.1"]])
+
+(defproject librarian-clojure "0.0.1-SNAPSHOT"
   :description "Book manager in Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-jetty-adapter "1.0.1"]])
+  :url "http://github.com/jaceklaskowski/librarian-clojure"
+  :repositories [["sonatype-snapshots"
+                  "https://oss.sonatype.org/content/repositories/snapshots/"]]
+  :dependencies ~(conj common-deps
+                       '[org.clojure/clojure "1.3.0"])
+  :multi-deps {"1.4.0" ~(conj common-deps
+                       '[org.clojure/clojure "1.4.0-master-SNAPSHOT"])}
+  :dev-dependencies [[lein-multi "1.1.0-SNAPSHOT"]
+                     [lein-ring "0.5.4"]]
+  :ring {:handler librarian-clojure.core/app})
--- src/librarian_clojure/core.clj
+++ src/librarian_clojure/core.clj
@@ -1,10 +1,24 @@
 (ns librarian-clojure.core
-  (:use ring.adapter.jetty))
+  (:use ring.adapter.jetty
+        ring.handler.dump))
 
-(defn app [req]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello, world"})
+(defn hello-world [app]
+  (fn [req]
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body "Hello, world"}))
+
+(defn upper-case [app]
+  (fn [req]
+    (let [resp (app req)
+          body (:body resp)
+          upper-case-body (.toUpperCase body)]
+      (conj resp {:body upper-case-body}))))
+  
+(def app
+  (-> handle-dump
+    hello-world
+    upper-case))
 
 (defn -main [port]
   (run-jetty app {:port (Integer. port)}))
