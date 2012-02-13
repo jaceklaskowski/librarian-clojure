5303c15b975d69de9085ce681dc555e7b6845b08
Jacek Laskowski <jacek@japila.pl>
2012-02-13 20:09:45 +0100

    Fix require imports

--- src/librarian_clojure/repl.clj
+++ src/librarian_clojure/repl.clj
@@ -3,8 +3,8 @@
   "The starting namespace for the project. This is the namespace that
   users will land in when they start a Clojure REPL. It exists to
   provide convenience functions like 'go' and 'dev-server'."  
-  (:require [[clojure.java.browse :as browse]
-             [librarian-clojure.core :as core]]))
+  (:require [clojure.java.browse :as browse]
+            [librarian-clojure.core :as core]))
 
 (defn go
   "Start a browser-connected REPL and launch a browser to talk to it."
