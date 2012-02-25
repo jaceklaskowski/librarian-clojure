e7d991587eebcc750e9b46118076f5bd6d6e145b
Jacek Laskowski <jacek@japila.pl>
2012-02-25 23:05:12 +0100

    Add lazytest so lein midje --lazytest works

--- project.clj
+++ project.clj
@@ -4,14 +4,16 @@
                    [ring/ring-devel "1.0.1"]
                    [compojure "1.0.1"]
                    [hiccup "0.3.8"]
-                   [congomongo "0.1.8"]])
+                   [congomongo "0.1.8"]
+                   [midje "1.3.1"]])
 
 ;; project definition for multi-version testing - consult :multi-deps option
 (defproject librarian-clojure "0.0.1-SNAPSHOT"
   :description "Book manager in Clojure"
   :url "http://github.com/jaceklaskowski/librarian-clojure"
   :repositories [["sonatype-snapshots"
-                  "https://oss.sonatype.org/content/repositories/snapshots/"]]
+                  "https://oss.sonatype.org/content/repositories/snapshots/"]
+                 ["stuart" "http://stuartsierra.com/maven2"]]
   :dependencies ~(conj common-deps
                        '[org.clojure/clojure "1.3.0"])
   :repl-init librarian-clojure.repl
@@ -22,7 +24,8 @@
                      [lein-eclipse "1.0.0"]
                      [ring-mock "0.1.1"]
                      [midje "1.3.1"]
-                     [lein-midje "1.0.8"]]
+                     [lein-midje "1.0.8"]
+                     [com.stuartsierra/lazytest "1.2.3"]]
   :ring {:handler librarian-clojure.core/app
          :init librarian-clojure.run/run-local}
   :main librarian-clojure.run
