d3dc28daf6c5cb4f3c5e7317229a89119f8137a0
Jacek Laskowski <jacek@japila.pl>
2012-04-20 09:45:30 +0200

    Use shorter syntax for dynamic vars

--- src/librarian_clojure/repl.clj
+++ src/librarian_clojure/repl.clj
@@ -6,7 +6,7 @@
   (:require [clojure.java.browse :as browse]
             [librarian-clojure.core :as core]))
 
-(def ^{:dynamic true} *server*)
+(def ^:dynamic *server*)
 
 (defn go
   "Start a browser-connected REPL and launch a browser to talk to it."
