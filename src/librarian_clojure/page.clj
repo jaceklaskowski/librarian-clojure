a8bcb5542809c99251bfc6aef12701ba3407f690
Jacek Laskowski <jacek@japila.pl>
2012-04-20 21:03:16 +0200

    Use require not use - make it compilable

--- src/librarian_clojure/page.clj
+++ src/librarian_clojure/page.clj
@@ -1,8 +1,6 @@
 (ns librarian-clojure.page
-  (:use [hiccup.page :only (html5)]
-        [clojure.string :only (replace)])
-  (:require [hiccup.core :as h]))
+  (:require [clojure.string :as s]))
 
 (defn render-login []
   (let [template (slurp (.getResourceAsStream Object "/public/index.html"))]
-    (replace template #"lib-booklist" "s = 'Hello world'")))
+    (s/replace template #"lib-booklist" "s = 'Hello world'")))
