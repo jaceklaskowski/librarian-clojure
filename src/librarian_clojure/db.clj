268b3d4734788aaa296833ac2e988e8d4c71da3c
Jacek Laskowski <jacek@japila.pl>
2012-12-25 20:25:08 +0100

    No def inside functions - slightly better but still awkward how the db connection is managed

--- src/librarian_clojure/db.clj
+++ src/librarian_clojure/db.clj
@@ -2,23 +2,27 @@
   (:use somnium.congomongo)
   (:import [jBCrypt BCrypt]))
 
+(def ^:dynamic *database-connection* nil)
+
 (defn init [env]
   (println "Environment: " env)
   (if (= env :heroku)
     (do
-      (def db 
-        (make-connection :app2623043
-                         {:host "staff.mongohq.com" :port 10056}))
+      (alter-var-root #'*database-connection* 
+                      (fn [_] 
+                        (make-connection :app2623043 {:host "staff.mongohq.com" :port 10056})))
       (println "Database URL: " (System/getenv "DATABASE_URL"))
-      (println "Authentication result: " (authenticate db "heroku" "passw0rd")))
+      (println "Authentication result: " (authenticate *database-connection* "heroku" "passw0rd"))))
     (do
-      (def db
-        (make-connection :test
-                         {:host "127.0.0.1" :port 27017})))))
+      (alter-var-root #'*database-connection*
+                      (fn [_]
+                        (make-connection :test {:host "127.0.0.1" :port 27017})))))
 
 (defn- next-seq [coll]
-  (with-mongo db
-    (:seq (fetch-and-modify :sequences {:_id coll} {:$inc {:seq 1}} :return-new? true :upsert? true))))
+  (when *database-connection*
+    (with-mongo *database-connection*
+      (:seq (fetch-and-modify :sequences {:_id coll} {:$inc {:seq 1}}
+                              :return-new? true :upsert? true)))))
 
 (defn- insert-with-id [coll el]
   (insert! coll (assoc el :_id (next-seq coll))))
@@ -26,26 +30,30 @@
 ;; Books
 
 (defn db-add-book [book]
-  (with-mongo db
-    (insert-with-id :books book)))
+  (when *database-connection*
+    (with-mongo *database-connection*
+      (insert-with-id :books book))))
 
 (defn db-get-books
   ([]
     (db-get-books {}))
   ([query]
-    (with-mongo db
-      (fetch :books :where query))))
+    (when *database-connection*
+      (with-mongo *database-connection*
+        (fetch :books :where query)))))
 
 (defn db-get-book [id] 
   (first (db-get-books {:_id id})))
 
 (defn db-update-book [id book] 
-  (with-mongo db
-    (update! :books {:_id id} book)))
+  (when *database-connection*
+    (with-mongo *database-connection*
+      (update! :books {:_id id} book))))
 
 (defn db-delete-book [id]
-  (with-mongo db
-    (destroy! :books {:_id id})))
+  (when *database-connection*
+    (with-mongo *database-connection*
+      (destroy! :books {:_id id}))))
 
 ;; Users
 
@@ -53,12 +61,13 @@
   ([login password]
     (db-add-user login password []))
   ([login password roles]
-    (with-mongo db
-      (insert! :users {:login login :password password :roles (concat [:user] roles)}))))
+    (when *database-connection*
+      (with-mongo *database-connection*
+        (insert! :users {:login login :password password :roles (concat [:user] roles)})))))
 
 (defn db-get-user [login]
-  (when login
-    (with-mongo db
+  (when (and login *database-connection*)
+    (with-mongo *database-connection*
       (if-let [user (fetch-one :users :where {:login login})]
         (let [roles-kwset (set (map keyword (:roles user)))]
           (assoc user :roles roles-kwset))))))
