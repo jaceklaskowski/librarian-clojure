4874435db35e5d203ef4ef48175c55faf2b87e7b
Jacek Laskowski <jacek@japila.pl>
2012-12-26 20:44:00 +0100

    Facts about page confirmed (ran fine)

--- test/librarian_clojure/test/t_page.clj
+++ test/librarian_clojure/test/t_page.clj
@@ -3,12 +3,16 @@
         librarian-clojure.security
         midje.sweet))
 
-(fact "render-main renders admin page"
-      (against-background (has-role? :admin) => true
-                          (render-admin) => "admin")
-      (render-main) => "admin")
+(fact "render-main renders admin page when authenticated as admin"
+  (let [request {}]
+    (render-main request) => "admin"
+    (provided
+      (has-role? request :admin) => true
+      (render-admin request) => "admin")))
 
-(fact "render-main renders user page"
-      (against-background (has-role? :admin) => false
-                          (render-user) => "user")
-      (render-main) => "user")
+(fact "render-main renders user page for non-admin users"
+  (let [request {}]
+    (render-main request) => "user"
+    (provided
+      (has-role? request :admin) => false
+      (render-user request) => "user")))
