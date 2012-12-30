be7c1bd525ffa72c146c7eafd653b9d14f13e2b6
Jacek Laskowski <jacek@japila.pl>
2012-12-30 23:53:56 +0100

    Enlive's in! index.html becomes a template (very rudimentary for now though)

--- src/librarian_clojure/page.clj
+++ src/librarian_clojure/page.clj
@@ -2,7 +2,8 @@
   (:use [librarian-clojure books])
   (:require [clojure.string :as s]
             [clojure.data.json :as json]
-            [librarian-clojure.security :as sec]))
+            [librarian-clojure.security :as sec]
+            [net.cgrand.enlive-html :as html]))
 
 (defn read-file [path]
   (slurp (.getResourceAsStream Object path)))
@@ -13,7 +14,7 @@
 (defn greet-user [{:keys [login]}]
   (str "Hello, " login))
 
-(defn render-user [request]
+(defn Xrender-user [request]
   (let [template (read-file "/public/index.html")]
     (set-attr template "lib-booklist"  (str "var books = " (-> (get-books) json/json-str)))
     (if-let [user (sec/get-user request)]
@@ -24,6 +25,13 @@
         (set-attr "login-form" (read-file "/public/login_form.chtml"))
         (set-attr "logout-form" "")))))
 
+(html/deftemplate index "public/index.html"
+  [ctxt]
+  [:p#message] (html/content (get ctxt :message "Nothing to see here")))
+
+(defn render-user [request]
+  (index {:message "Enlive's here!"}))
+
 (defn render-admin [request]
   (let [template (read-file "/public/admin.html")]
     (-> template
@@ -32,6 +40,6 @@
       (set-attr "logout-form" (read-file "/public/logout-form.html")))))
 
 (defn render-main [request]
-  (if (sec/has-role? request :admin)
+  (if (-> request (sec/has-role? :admin))
     (render-admin request)
     (render-user request)))
