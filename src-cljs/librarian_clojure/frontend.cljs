06b1faba6a24c1c3d080143bdb54e146aab6f1ae
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2013-01-02 21:43:35 +0100

    First (almost) fully tested real function added

--- src-cljs/librarian_clojure/frontend.cljs
+++ src-cljs/librarian_clojure/frontend.cljs
@@ -1,13 +1,22 @@
 (ns librarian-clojure.frontend
-  (:use [jayq.core :only [$]]))
+  (:use [jayq.core :only [$]]
+        [jayq.util :only [log]])
+  (:require [jayq.core :as jq]))
 
 (defn hello [x]
   (js/alert (str "Hello " x "!")))
 
 (defn create-button
-  [& {:keys [button-class]}]
+  [& {:keys [button-class icon caption callback]}]
   (let [button ($ "<button>")]
     (when button-class 
-      (.addClass button (name button-class)))
+      (jq/add-class button (name button-class)))
+    (when icon
+      (jq/append button (-> ($ "<i>") (jq/add-class (name icon))))
+      (jq/append button " "))
+    (when caption
+      (jq/append button caption))
+    (when callback
+      (jq/click button callback))
     button))
       
--- test-cljs/librarian_clojure/test/frontend.cljs
+++ test-cljs/librarian_clojure/test/frontend.cljs
@@ -1,6 +1,8 @@
 (ns librarian-clojure.test.frontend
   (:use [jayq.core :only [$]])
-  (:require [librarian-clojure.frontend :as frontend])
+  (:require [librarian-clojure.frontend :as frontend]
+            [jayq.core :as jq]
+            [clojure.string :as str])
   (:require-macros [librarian-clojure.test.macro-utils :as m]))
 
 (defn jqdom [jqnode]
@@ -10,9 +12,18 @@
   (.isEqualNode (jqdom jqnode1) (jqdom jqnode2)))
 
 (m/deftest "(create-button) returns an empty button"
-  (js/ok (jqdom= (frontend/create-button) ($ "<button>")), "Should return empty button"))
+  (js/ok (jqdom= (frontend/create-button) ($ "<button>")), "Returns empty button"))
 
-(m/deftest "(create-button :class :foo) returns button with class foo"
+(m/deftest "(create-button :button-class :foo) returns button with class foo"
   (let [button (jqdom (frontend/create-button :button-class :foo))]
-    (js/ok (== button/tagName "BUTTON") "This is not a button!")
-    (js/ok (== button/className "foo") "Button does not have class foo!")))
+    (js/ok (== button/tagName "BUTTON") "This is a button!")
+    (js/ok (== button/className "foo") "Button does have class foo!")))
+
+(m/deftest "(create-button :button-class :foo :caption \"Foo\" :icon :icon-class) returns according button
+TODO: add callback test"
+  (let [jbutton (frontend/create-button :button-class :foo :caption "Foo" :icon :icon-class)
+        button (jqdom jbutton)]
+    (js/ok (== button/tagName "BUTTON") "This is a button!")
+    (js/ok (== button/className "foo") "Button does have class foo!")
+    (js/ok (-> jbutton (jq/children "i") first $ (jq/has-class "icon-class")) "There's an icon!")
+    (js/ok (->> jbutton jq/text (re-find #"Foo")))))
