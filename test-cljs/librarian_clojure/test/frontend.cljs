d9b6707d78756965bf7b193175a41c4704feb112
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2013-01-02 23:04:09 +0100

    A bit more tests exploring various aspects like callbacks etc.

--- test-cljs/librarian_clojure/test/frontend.cljs
+++ test-cljs/librarian_clojure/test/frontend.cljs
@@ -1,8 +1,7 @@
 (ns librarian-clojure.test.frontend
   (:use [jayq.core :only [$]])
   (:require [librarian-clojure.frontend :as frontend]
-            [jayq.core :as jq]
-            [clojure.string :as str])
+            [jayq.core :as jq])
   (:require-macros [librarian-clojure.test.macro-utils :as m]))
 
 (defn jqdom [jqnode]
@@ -11,19 +10,37 @@
 (defn jqdom= [jqnode1 jqnode2]
   (.isEqualNode (jqdom jqnode1) (jqdom jqnode2)))
 
+(def ok js/ok)
+
+(defn not-ok [assertion comment] (js/ok (not assertion) comment))
+
 (m/deftest "(create-button) returns an empty button"
   (js/ok (jqdom= (frontend/create-button) ($ "<button>")), "Returns empty button"))
 
 (m/deftest "(create-button :button-class :foo) returns button with class foo"
   (let [button (jqdom (frontend/create-button :button-class :foo))]
-    (js/ok (== button/tagName "BUTTON") "This is a button!")
-    (js/ok (== button/className "foo") "Button does have class foo!")))
+    (ok (== button/tagName "BUTTON") "This is a button!")
+    (ok (== button/className "foo") "Button does have class foo!")))
 
-(m/deftest "(create-button :button-class :foo :caption \"Foo\" :icon :icon-class) returns according button
-TODO: add callback test"
+(m/deftest "(create-button :button-class :foo :caption \"Foo\" :icon :icon-class) returns according button"
   (let [jbutton (frontend/create-button :button-class :foo :caption "Foo" :icon :icon-class)
         button (jqdom jbutton)]
-    (js/ok (== button/tagName "BUTTON") "This is a button!")
-    (js/ok (== button/className "foo") "Button does have class foo!")
-    (js/ok (-> jbutton (jq/children "i") first $ (jq/has-class "icon-class")) "There's an icon!")
-    (js/ok (->> jbutton jq/text (re-find #"Foo")))))
+    (ok (== button/tagName "BUTTON") "This is a button!")
+    (ok (== button/className "foo") "Button does have class foo!")
+    (ok (-> jbutton (jq/children "i") first $ (jq/has-class "icon-class")) "There's an icon!")
+    (ok (->> jbutton jq/text (re-find #"Foo")) "There's a caption!")))
+
+(m/deftest-async "(create-button :callback ...) test" 1
+  (let [button (frontend/create-button :callback (fn [] (ok true "Called once!") (js/start)))]
+    (jq/trigger button :click)))
+
+(m/deftest "(existing-book? ...)"
+  (ok (frontend/existing-book? (-> "<span>" $ (jq/attr "data-id" "foo"))) "True when has data-id")
+  (not-ok (frontend/existing-book? ($ "<span>")) "False when not"))
+
+(m/deftest "(get-edited-book ...) test"
+  (let [row ($ "<div data-id=\"foo\"><input name=\"author\" value=\"bar\"><input name=\"title\" value=\"baz\"></div>")
+        res (frontend/get-edited-book row)]
+    (ok (== (:_id res) "foo") "Has :id")
+    (ok (== (:author res) "bar") "Has :author")
+    (ok (== (:title res) "baz")) "Has :title"))
--- test-cljs/librarian_clojure/test/macro_utils.clj
+++ test-cljs/librarian_clojure/test/macro_utils.clj
@@ -2,3 +2,6 @@
 
 (defmacro deftest [name & body] `(js/test ~name (fn [] ~@body)))
 
+(defmacro deftest-async 
+  ([name expected & body] `(js/asyncTest ~name ~expected (fn [] ~@body))))
+
