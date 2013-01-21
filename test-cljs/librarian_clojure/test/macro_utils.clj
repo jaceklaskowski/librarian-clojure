e9fc16ca9ec9a76009fb74fef0390aed4d09c446
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2013-01-21 23:58:00 +0100

    Further work on converting js codebase to cljs.

--- test-cljs/librarian_clojure/test/macro_utils.clj
+++ test-cljs/librarian_clojure/test/macro_utils.clj
@@ -5,3 +5,10 @@
 (defmacro deftest-async 
   ([name expected & body] `(js/asyncTest ~name ~expected (fn [] ~@body))))
 
+(defmacro redef-fn [fn-binding fn-val & body]
+  (let [old-binding (-> fn-binding name gensym)] 
+    `(try
+       ~(list 'set! fn-binding fn-val)
+       ~@body
+       (finally 
+         ~(list 'set! fn-binding old-binding)))))
